7176b29e14800e8b7a8df83349ab3bed30f2a4d6
Sankov Alex <73057920+Alexsa7161@users.noreply.github.com>
2025-09-02 19:47:31 +0300

    Update FitnessServerApplication.java

--- src/main/java/com/example/fitnessserver/FitnessServerApplication.java
+++ src/main/java/com/example/fitnessserver/FitnessServerApplication.java
@@ -2,6 +2,8 @@ package com.example.fitnessserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 public class FitnessServerApplication {
