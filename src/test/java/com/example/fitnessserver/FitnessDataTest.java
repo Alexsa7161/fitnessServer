7c0a5278c83e9461a9aa2808df0665b2ade7d9fd
Sankov Alex <73057920+Alexsa7161@users.noreply.github.com>
2025-09-09 21:04:32 +0300

    Create FitnessDataTest.java

---
+++ src/test/java/com/example/fitnessserver/FitnessDataTest.java
@@ -0,0 +1,58 @@
+package com.example.fitnessserver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FitnessDataTest {
+
+    @Test
+    public void testAllArgsConstructorAndGetters() {
+        FitnessData data = new FitnessData(1L, "user_123", "steps", 1000.0, 123456789L);
+
+        assertEquals(1L, data.getId());
+        assertEquals("user_123", data.getUserId());
+        assertEquals("steps", data.getMetric());
+        assertEquals(1000.0, data.getValue());
+        assertEquals(123456789L, data.getTimestamp());
+    }
+
+    @Test
+    public void testNoArgsConstructorAndSetters() {
+        FitnessData data = new FitnessData();
+        data.setId(2L);
+        data.setUserId("user_456");
+        data.setMetric("calories");
+        data.setValue(250.5);
+        data.setTimestamp(987654321L);
+
+        assertEquals(2L, data.getId());
+        assertEquals("user_456", data.getUserId());
+        assertEquals("calories", data.getMetric());
+        assertEquals(250.5, data.getValue());
+        assertEquals(987654321L, data.getTimestamp());
+    }
+
+    @Test
+    public void testEqualsAndHashCode() {
+        FitnessData d1 = new FitnessData(1L, "user_1", "steps", 1000.0, 111L);
+        FitnessData d2 = new FitnessData(1L, "user_1", "steps", 1000.0, 111L);
+        FitnessData d3 = new FitnessData(2L, "user_2", "calories", 200.0, 222L);
+
+        assertEquals(d1, d2);
+        assertEquals(d1.hashCode(), d2.hashCode());
+
+        assertNotEquals(d1, d3);
+        assertNotEquals(d1.hashCode(), d3.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        FitnessData data = new FitnessData(1L, "user_123", "steps", 1000.0, 123456789L);
+        String str = data.toString();
+
+        assertTrue(str.contains("user_123"));
+        assertTrue(str.contains("steps"));
+        assertTrue(str.contains("1000.0"));
+    }
+}
