bfbc552edb949467cdcfa6d5b5d55e42797dd2cc
Sankov Alex <73057920+Alexsa7161@users.noreply.github.com>
2025-09-14 18:02:30 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import * as reportWebVitalsModule from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 
-describe('index.js', () => {
-  let div;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-  beforeEach(() => {
-    div = document.createElement('div');
-    div.id = 'root';
-    document.body.appendChild(div);
-  });
-
-  afterEach(() => {
-    document.body.removeChild(div);
-    jest.restoreAllMocks();
-  });
-
-  test('рендерит App и вызывает reportWebVitals', () => {
-    // Шпионим за reportWebVitals
-    const spy = jest.spyOn(reportWebVitalsModule, 'default').mockImplementation(() => {});
-
-    // Импорт index.js динамически, чтобы вызов reportWebVitals сработал
-    const indexModule = require('./index.js');
-
-    expect(spy).toHaveBeenCalled();
-  });
-});
+// Вызываем reportWebVitals для измерения производительности
+reportWebVitals();
