59a2d00d29942d313a0468c38f92bbcf7d2ecb25
Sankov Alex <73057920+Alexsa7161@users.noreply.github.com>
2025-09-14 18:11:28 +0300

    Update useWebSocket.js

--- client/src/hooks/useWebSocket.js
+++ client/src/hooks/useWebSocket.js
@@ -1,27 +1,30 @@
 import { useEffect, useRef } from 'react';
 
 export const useWebSocket = (userId, onMessage) => {
-    const socketRef = useRef(null);
+  const socketRef = useRef(null);
 
-    useEffect(() => {
-        if (!userId) return;
+  useEffect(() => {
+    if (!userId) return;
 
-        const socket = new WebSocket('ws://localhost:8080/ws');
-        socket.onopen = () => socket.send(userId);
-        socket.onmessage = (e) => {
-            try {
-                const data = JSON.parse(e.data);
-                onMessage(data);
-            } catch (err) {
-                console.error("Ошибка при разборе JSON:", e.data);
-            }
-        };
-        socket.onerror = () => console.error("Ошибка WebSocket");
+    const socket = new WebSocket('ws://localhost:8080/ws');
 
-        socketRef.current = socket;
+    socket.onopen = () => socket.send(userId);
 
-        return () => socket.close();
-    }, [userId, onMessage]);
+    socket.onmessage = (e) => {
+      try {
+        const data = JSON.parse(e.data);
+        onMessage(data);
+      } catch (err) {
+        console.error("Ошибка при разборе JSON:", e.data);
+      }
+    };
 
-    return socketRef.current;
+    socket.onerror = () => console.error("Ошибка WebSocket");
+
+    socketRef.current = socket;
+
+    return () => socket.close();
+  }, [userId, onMessage]);
+
+  return socketRef.current;
 };
